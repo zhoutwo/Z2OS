05ab5c087f10076081f43be32d82b3fbbc071a0f
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-04-17 14:53:28 -0400

    Improve compileOS.sh

--- m1/compileOS.sh
+++ m1/compileOS.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+make clean
+cp ./test.img ./floppya.img
 nasm bootload.asm
 dd if=/dev/zero of=floppya.img bs=512 count=2880
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
-make clean
 make
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
 bochs -f opsys.bxrc
