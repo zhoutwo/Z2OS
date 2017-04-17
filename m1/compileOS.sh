30cc5bb295fc95cdd094e018c057aa21f4d3bcf5
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-04-17 11:27:29 -0400

    Add shebang to the beginning of shell script

--- m1/compileOS.sh
+++ m1/compileOS.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 nasm bootload.asm
 dd if=/dev/zero of=floppya.img bs=512 count=2880
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
@@ -5,3 +6,4 @@ make clean
 make
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
 bochs -f opsys.bxrc
+
