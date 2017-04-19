99d0d76ee2da1cad63233db8e8765e42de3a3953
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-04-19 10:05:33 -0400

    Port files from M2 into M3

---
+++ m3/compileOS.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+make clean
+cp ./test.img ./floppya.img
+nasm bootload.asm
+dd if=/dev/zero of=floppya.img bs=512 count=2880
+dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
+make
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+dd if=message.txt of=floppya.img bs=512 count=1 seek=30 conv=notrunc
+bochs -f opsys.bxrc
---
+++ m3/kernel.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void printString(char *);
+void readString(char *);
+void readSector(char *, int);
+int mod(int, int);
+int div(int, int);
+void handleInterrupt21(int, int, int, int);
+
+int main()
+{
+  int start;
+  int i;
+
+  char line[80];
+  printString("Enter a line: \0");
+  makeInterrupt21();
+  interrupt(0x21,1,line,0,0);
+  interrupt(0x21,0,line,0,0);
+  while(1){}
+  return 0;
+}
+
+void printString(char *str) {
+  unsigned int i = 0;
+  while (str[i] != '\0') {
+    interrupt(0x10, 0xe*256+str[i], 0, 0, 0);
+    i++;
+  }
+}
+
+void readString(char *buffer) {
+  char currentChar;
+  char bufferToPrint[3];
+  unsigned int i = 0;
+  bufferToPrint[1] = '\0';
+  do {
+    currentChar = interrupt(0x16, 0, 0, 0, 0);
+    switch (currentChar) {
+      case 0xd:
+        buffer[i] = 0xd;
+        i++;
+        buffer[i] = 0xa;
+        i++;
+        buffer[i] = 0x0;
+        i++;
+        bufferToPrint[0] = '\r';
+        bufferToPrint[1] = '\n';
+        bufferToPrint[2] = '\0';
+        printString(bufferToPrint);
+        return;
+      case 0x8:
+        bufferToPrint[0] = currentChar;
+        if (i > 0) {
+          printString(bufferToPrint);
+          i--;
+        }
+        break;
+      default:
+        buffer[i] = currentChar;
+        bufferToPrint[0] = currentChar;
+        printString(bufferToPrint);
+        i++;
+    }
+  } while (1);
+}
+
+void readSector(char *buffer, int sector) {
+  int relativeSector = mod(sector, 18) + 1;
+  int head = mod(div(sector, 18), 2);
+  int track = div(sector, 36);
+  interrupt(0x13, 2*256+1, buffer, track*256+relativeSector, head*256+0);
+}
+
+int mod(int a, int b) {
+  while (a >= b) {
+    a = a - b;
+  }
+  return a;
+}
+
+int div(int a, int b) {
+  int quotient = 0;
+  while ((quotient + 1) * b <= a) {
+    quotient++;
+  }
+  return quotient;
+}
+
+void handleInterrupt21(int ax, int bx, int cx, int dx) {
+  switch (ax) {
+    case 0:
+      printString(bx);
+      break;
+    case 1:
+      readString(bx);
+      break;
+    case 2:
+      readSector(bx, cx);
+      break;
+    default:
+      printString("Error: What did you call it with?\r\n\0");
+  }
+}
