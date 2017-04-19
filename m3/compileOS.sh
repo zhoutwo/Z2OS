f3d56d467a8ced0f992a3865b0d6cd14201f806d
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-04-19 10:10:49 -0400

    M3 step 5

--- m3/compileOS.sh
+++ m3/compileOS.sh
@@ -8,7 +8,8 @@ dd if=/dev/zero of=floppya.img bs=512 count=2880
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 make
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
-dd if=message.txt of=floppya.img bs=512 count=1 seek=30 conv=notrunc
 dd if=map.img of=floppya.img bs=512 count=1 seek=1 conv=notrunc
 dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
+gcc -o loadFile loadFile.c
+./loadFile message.txt
 bochs -f opsys.bxrc
