3c91f38897dd3c82460d8fbb078c98d18d9f7893
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-04-26 21:40:21 -0400

    Small modifications

--- m4/shell.c
+++ m4/shell.c
@@ -10,47 +10,46 @@ void cleanFilename(char*);
 int main()
 {
   char buffer[READSTRING_MAX_BUFFER_SIZE];
-  char *filename;
+  char *filename1, *filename2, *filename3, *filename4;
   char fileContentBuffer[MAXIMUM_FILE_SIZE];
+  char debug[2];
+  debug[1] = '\0';
   while(1) {
-<<<<<<< HEAD
-    interrupt(0x21, 0, "SHELL> ", 0, 0);
-    interrupt(0x21, 1, buffer, 0, 0);
-    if (strncmp(buffer, "type", 4)) {
-=======
     /* Routine buffer cleanup */
     buffer[0] = '\0';
-    filename = 0;
+    filename1 = 0;
     fileContentBuffer[0] = '\0';
 
     /* Actual code */
     interrupt(0x21, 0, "SHELL> ", 0, 0);
     interrupt(0x21, 1, buffer, 0, 0);
     if (strncmp(buffer, "type ", 5)) {
->>>>>>> M4 step 3
-      filename = buffer + 5;
-      cleanFilename(filename);
-      interrupt(0x21, 3, filename, fileContentBuffer, 0);
+      filename1 = buffer + 5;
+      cleanFilename(filename1);
+      interrupt(0x21, 3, filename1, fileContentBuffer, 0);
       interrupt(0x21, 0, fileContentBuffer, 0, 0);
-<<<<<<< HEAD
-    } else if (strncmp(buffer, "execute", 7)) {
-      filename = buffer + 8;
-      cleanFilename(filename);
-      interrupt(0x21, 4, filename, 0x2000, 0);
-    } else if (strncmp(buffer, "exit", 4)) {
-      return 0;
-=======
     } else if (strncmp(buffer, "execute ", 8)) {
-      filename = buffer + 8;
-      cleanFilename(filename);
-      interrupt(0x21, 4, filename, 0x2000, 0);
-    } else if (strncmp(buffer, "exit ", 5)) {
+      filename1 = buffer + 8;
+      cleanFilename(filename1);
+      interrupt(0x21, 4, filename1, 0x2000, 0);
+    } else if (strncmp(buffer, "exit\r", 5)) {
       return 0;
     } else if (strncmp(buffer, "delete ", 7)) {
-      filename = buffer + 7;
-      cleanFilename(filename);
-      interrupt(0x21, 7, filename, 0, 0);
->>>>>>> M4 step 3
+      filename1 = buffer + 7;
+      cleanFilename(filename1);
+      interrupt(0x21, 7, filename1, 0, 0);
+    } else if (strncmp(buffer, "copy ", 5)) {
+      filename1 = buffer + 5;
+      debug[0] = indexOf(filename1, " ") + 0x30;
+      interrupt(0x21, 0, debug, 0, 0);
+      filename1[indexOf(filename1, " ")] = '\0';
+      interrupt(0x21, 0, filename1, 0, 0);
+      filename2 = indexOf(buffer + 5, " ") + 1;
+      interrupt(0x21, 0, filename2, 0, 0);
+      interrupt(0x21, 3, filename1, fileContentBuffer, 0);
+      interrupt(0x21, 8, filename2, fileContentBuffer, 0);
+    } else if (strncmp(buffer, "dir\r", 4)) {
+    } else if (strncmp(buffer, "create ", 7)) {
     } else {
       interrupt(0x21, 0, "Bad Command!\r\n", 0, 0);
     }
