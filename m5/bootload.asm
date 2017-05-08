905398c593e224a00ee71234dba69345394abbe4
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-08 11:39:02 -0400

    Make kernel maximum size

--- m5/bootload.asm
+++ m5/bootload.asm
@@ -5,7 +5,7 @@
 
 	bits 16
 KSEG	equ	0x1000		;kernel goes into memory at 0x10000
-KSIZE	equ	11		;kernel is at most 11 sectors (and probably less)
+KSIZE	equ	26		;kernel is at most 26 sectors (and probably less)
 KSTART	equ	3		;kernel lives at sector 3 (makes room for map & dir)
 
 	;boot loader starts at 0 in segment 0x7c00
